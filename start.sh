4a9b6d957e7b7a3600f28a2574564fe94263ba18
tuearnings <136893874+tuearnings@users.noreply.github.com>
2023-06-27 11:37:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/kiran6412/tuonlinebacup1.git /tuonlinebacup1
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /tuonlinebacup1
+  git clone $SOURCE_CODE /URL-Shortener-V2backup
 fi
 cd /tuonlinebacup1
 pip3 install -U -r requirements.txt
