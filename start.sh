25235cb660572bb031d9fbd16751c2591a8a44a5
tuearnings <136893874+tuearnings@users.noreply.github.com>
2023-06-27 11:12:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kiran6412/URL-Shortener-V2backup.git /URL-Shortener-V2backup
+  git clone https://github.com/kiran6412/tuonlinebacup1.git /tuonlinebacup1
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2backup
+  git clone $SOURCE_CODE /tuonlinebacup1
 fi
-cd /URL-Shortener-V2backup
+cd /tuonlinebacup1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
